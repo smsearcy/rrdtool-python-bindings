9d3950f043ac9d62b34f7e63f0f656d06105bc1f
Antoine Nguyen <tonio@ngyn.org>
2025-09-14 10:20:27 +0200

    Fixed build issue on Debian 13

--- setup.py
+++ setup.py
@@ -5,9 +5,15 @@ import subprocess
 
 from setuptools import Extension, setup
 
+extra_compile_args = []
+
 # on Fedora it wanted `gcc-11`, which didn't exist
 if subprocess.run(["command -v gcc-11"], shell=True, check=False).returncode > 0:
     os.environ["CC"] = "gcc"
+
+# Debian Trixe: disable incompatible-pointer-types compilation errors for now...
+if not subprocess.run(["grep 13 /etc/debian_version"], shell=True, check=False).returncode:
+    extra_compile_args = ["-Wno-incompatible-pointer-types"]
 
 setup(
     ext_modules=[
@@ -15,6 +21,7 @@ setup(
             name="rrdtool",
             sources=["src/rrdtool/rrdtoolmodule.c"],
             libraries=["rrd"],
+            extra_compile_args=extra_compile_args,
         )
     ]
 )
