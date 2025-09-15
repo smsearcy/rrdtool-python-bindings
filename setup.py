46e1a5c19268fe4f349ea43aab194b9013a66626
Scott Searcy <smsearcy@gmail.com>
2025-09-15 13:41:21 -0700

    Revert Fedora "fix" and add guard for Linux
    Further testing indicates that it was a Homebrew issue

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 """Instructions for setuptools to build the rrdtool bindings."""
 
-import os
 import platform
-import subprocess
+import sys
 
 from setuptools import Extension, setup
 
-# on Fedora it wanted `gcc-11`, which didn't exist
-if subprocess.run(["command -v gcc-11"], shell=True, check=False).returncode > 0:
-    os.environ["CC"] = "gcc"
-
 extra_compile_args = []
 
-# Debian Trixie: disable incompatible-pointer-types
-distro_info = platform.freedesktop_os_release()
-if distro_info["ID"] == "debian" and distro_info["VERSION_ID"] == "13":
-    extra_compile_args.append("-Wno-incompatible-pointer-types")
+if sys.platform.startswith("linux"):
+    distro_info = platform.freedesktop_os_release()
+    if distro_info["ID"] == "debian" and distro_info["VERSION_ID"] == "13":
+        # Debian Trixie: disable incompatible-pointer-types
+        extra_compile_args.append("-Wno-incompatible-pointer-types")
 
 print("Extra compile args:", extra_compile_args)
 
