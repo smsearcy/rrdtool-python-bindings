ae850c4c613cb141ab5d1f830b5cf94acc2978a1
Scott Searcy <smsearcy@gmail.com>
2025-09-15 13:24:13 -0700

    Add compiler flag to compile on Debian Trixie
    Debian 13 (Trixie) still has RRDtool 1.7.2 which doesn't include the fix
    from RRDtool 1.9 to address pointer type mismatches.
    Therefore, we need to build with `-Wno-incompatible-pointer-types`.

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 """Instructions for setuptools to build the rrdtool bindings."""
 
 import os
+import platform
 import subprocess
+import sys
 
 from setuptools import Extension, setup
 
@@ -9,12 +11,27 @@ from setuptools import Extension, setup
 if subprocess.run(["command -v gcc-11"], shell=True, check=False).returncode > 0:
     os.environ["CC"] = "gcc"
 
+extra_compile_args = []
+
+# Debian Trixie: disable incompatible-pointer-types
+if sys.version_info >= (3, 10):
+    distro_info = platform.freedesktop_os_release()
+    if distro_info["ID"] == "debian" and distro_info["VERSION_ID"] == "13":
+        extra_compile_args.append("-Wno-incompatible-pointer-types")
+elif not subprocess.run(
+    ["/usr/bin/grep", "13", "/etc/debian_version"], check=False
+).returncode:
+    extra_compile_args.append("-Wno-incompatible-pointer-types")
+
+print("Extra compile args:", extra_compile_args)
+
 setup(
     ext_modules=[
         Extension(
             name="rrdtool",
             sources=["src/rrdtool/rrdtoolmodule.c"],
             libraries=["rrd"],
+            extra_compile_args=extra_compile_args,
         )
     ]
 )
